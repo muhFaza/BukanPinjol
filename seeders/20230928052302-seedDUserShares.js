049b9dea993fdd74caec313613af84390d88bc6f
muhFaza <fazamuhammad30@gmail.com>
2023-09-28 12:33:55 +0700

    seeding done

---
+++ seeders/20230928052302-seedDUserShares.js
@@ -0,0 +1,31 @@
+'use strict';
+const fs = require('fs')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    let data = JSON.parse(fs.readFileSync('./dataForSeeding/userShares.json', 'utf-8')).map(el => {
+      el.createdAt = el.updatedAt = new Date()
+      return el
+    })
+    return queryInterface.bulkInsert('UserShares', data, {})
+  },
+
+  down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('UserShares', null, {})
+  }
+};
